24fd86aac2c93cbfdcb07c72528932d46a057450
Jean Lam <jlam7995@gmail.com>
2023-03-30 14:19:03 -0400

    print multiple rows, not with curses

--- DDR.py
+++ DDR.py
@@ -1,20 +1,10 @@
 # import keyboard
 # from tkinter import *
+import curses
+from curses import wrapper
 import random
 import time
 from time import sleep
-
-def main():
-  # print('First line', end='\r')
-  # print('Second line', end='\r')
-  beatmapsets = beatmap()
-  for beatmapset in beatmapsets:
-    print(beatmapset, end='\r')
-    time.sleep(0.5)
-  #print('< ^ v >', end='\r')
-  #print('< ^ v  ', end='\r')
-  #print('foo')
-  #sleep(1)
 
 def beats():
   list = [random.randint(0,1), random.randint(0,1), random.randint(0,1), random.randint(0,1)]
@@ -29,14 +19,38 @@ def beats():
 
 def beatmap():
   beatmap = []
-  for x in range(0,32):
+  #temporarily commenting out the empties
+  for x in range(0,4):
     beatmap.append([0,0,0,0])
   for x in range(0,10):
     beatmap.append(beats())
   return beatmap
 
+def main(stdscr):
+  bm = beatmap()
+  bm.reverse()
+  #disables blinking cursor
+  curses.curs_set(0)
+  rows = []
+  for x in bm:
+    row = " ".join(map(str, x))
+    rows.append(row)
+    #print(row)
+    #print("foo")
+    y = 0
+    while y<len(rows):
+      if y % 10 == 9: 
+        print (rows[y])
+      y+=1
+    #stdscr.addstr(0,0,row)
+    #stdscr.refresh()
+    #print(x, end='\r')
+    #time.sleep(1)
 
-main()
+wrapper(main)
+
+#stdscr = curses.initscr()
+#main(stdscr)
 
 # tk = Tk()
 # tk.title("DDR Game")
